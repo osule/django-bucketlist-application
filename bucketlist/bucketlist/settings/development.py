2751fadad33a691cd26d556ab543c746ddae97f3
Oluwafemi Sule <oluwafemi.sule@andela.com>
2015-11-04 15:40:11 +0100

    (chore) bootstrap django; configure travis and coverage

---
+++ bucketlist/bucketlist/settings/development.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from django_envie.workroom import convertfiletovars
+
+convertfiletovars()
+
+from .base import *
+
+DEBUG = True
+
+INSTALLED_APPS += ('django_nose',)
+
+TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
---
+++ bucketlist/bucketlist/settings/testing.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+from .base import *
+
+DEBUG = True
+
+INSTALLED_APPS += ('django_nose')
+
+TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
