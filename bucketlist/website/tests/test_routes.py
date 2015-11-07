d1b35a667d4f5d84d865191bf8cd7149c0a3a4a7
Oluwafemi Sule <oluwafemi.sule@andela.com>
2015-11-07 12:11:17 +0100

    test signup route

--- bucketlist/website/tests/test_routes.py
+++ bucketlist/website/tests/test_routes.py
@@ -3,6 +3,10 @@
 from django.core.urlresolvers import reverse
 from django.contrib.auth.models import User
 from django.test import TestCase, Client
+from faker import Faker
+
+
+fakerInst = Faker()
 
 
 class UserAuthTestCase(TestCase):
@@ -30,5 +34,20 @@ class UserAuthTestCase(TestCase):
         """
         response = self.client.get(
             reverse('app.logout')
+        )
+        self.assertEqual(response.status_code, 302)
+
+    def test_that_user_can_signup(self):
+        """tests that user can signup for a new account
+        """
+        response = self.client.post(
+            reverse('app.signup'),
+            data={
+                'username': fakerInst.user_name(),
+                'first_name': fakerInst.first_name(),
+                'last_name': fakerInst.last_name(),
+                'email': fakerInst.email(),
+                'password': fakerInst.password()
+            }
         )
         self.assertEqual(response.status_code, 302)
