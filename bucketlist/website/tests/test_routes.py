8973850632ea1210ea6c35d75abb92f504e39cf6
Oluwafemi Sule <oluwafemi.sule@andela.com>
2015-11-08 14:51:14 +0100

    test POST to bucketlist

--- bucketlist/website/tests/test_routes.py
+++ bucketlist/website/tests/test_routes.py
@@ -9,23 +9,28 @@ from faker import Faker
 fakerInst = Faker()
 
 
-class UserAuthTestCase(TestCase):
-    """Test that the user can login to access the service
+class SetUpMixin(object):
+    """Mixes the setUp for TestCases
     """
-
     def setUp(self):
         User.objects.create(username="basil", password="12345")
+        self.login_data = {
+            'username': 'basil',
+            'password': '12345'
+        }
         self.client = Client()
+
+
+class UserAuthTestCase(SetUpMixin, TestCase):
+    """Test that the user can login to access the service
+    """
 
     def test_that_user_can_login(self):
         """tests that the user can login to his/her account
         """
         response = self.client.post(
             reverse('app.login'),
-            data={
-                'username': 'basil',
-                'password': '12345'
-                }
+            data=self.login_data
         )
         self.assertEqual(response.status_code, 302)
 
@@ -51,3 +56,22 @@ class UserAuthTestCase(TestCase):
             }
         )
         self.assertEqual(response.status_code, 302)
+
+
+class BucketlistTestCase(SetUpMixin, TestCase):
+    """Test that the user can login and make a bucketlist
+    """
+    def test_that_user_can_post_to_bucketlist_route(self):
+        response = self.client.post(
+                reverse('app.login'),
+                data=self.login_data
+            )
+        self.assertEqual(response.status_code, 302)
+
+        bucketlist_data = {'name': 'Before I get married'}
+        response = self.client.post(
+                reverse('app.bucketlist'),
+                data=bucketlist_data
+            )
+        self.assertEqual(response.status_code == 302)
+        self.assertIn(bucketlist_data['name'], response.body)
