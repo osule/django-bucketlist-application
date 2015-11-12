3a8de8252d92a94a2de05ea2c6ff345c2dafcc41
Oluwafemi Sule <oluwafemi.sule@andela.com>
2015-11-13 00:21:00 +0100

    (feature) tests bucketlistitem can be CRUD

--- bucketlist/website/tests/test_routes.py
+++ bucketlist/website/tests/test_routes.py
@@ -12,13 +12,15 @@ fakerInst = Faker()
 class SetUpMixin(object):
     """Mixes the setUp for TestCases
     """
-    def setUp(self):
-        User.objects.create(username="basil", password="12345")
-        self.login_data = {
+    @classmethod
+    def setUpClass(cls):
+        super(SetUpMixin, cls).setUpClass()
+        cls.login_data = {
             'username': 'basil',
-            'password': '12345'
+            'password': 'some_really_strong_password'
         }
-        self.client = Client()
+        User.objects.create_user(**cls.login_data)
+        cls.client = Client()
 
 
 class UserAuthTestCase(SetUpMixin, TestCase):
@@ -26,7 +28,7 @@ class UserAuthTestCase(SetUpMixin, TestCase):
     """
 
     def test_that_user_can_login(self):
-        """tests that the user can login to his/her account
+        """Ensures that the user can login to his/her account
         """
         response = self.client.post(
             reverse('app.login'),
@@ -35,7 +37,7 @@ class UserAuthTestCase(SetUpMixin, TestCase):
         self.assertEqual(response.status_code, 302)
 
     def test_that_user_can_logout(self):
-        """tests that user can logout from his/her account
+        """Ensures that user can logout from his/her account
         """
         response = self.client.get(
             reverse('app.logout')
@@ -43,7 +45,7 @@ class UserAuthTestCase(SetUpMixin, TestCase):
         self.assertEqual(response.status_code, 302)
 
     def test_that_user_can_signup(self):
-        """tests that user can signup for a new account
+        """Ensures that user can signup for a new account
         """
         response = self.client.post(
             reverse('app.signup'),
@@ -57,21 +59,238 @@ class UserAuthTestCase(SetUpMixin, TestCase):
         )
         self.assertEqual(response.status_code, 302)
 
+    def test_that_user_cannot_signup_with_a_registered_username(self):
+        """Ensures that user cannot sign up with a registered username
+        """
+        response = self.client.post(
+            reverse('app.signup'),
+            {
+                'username': 'basil',
+                'password': 'do_not_know_what_goes_here'
+            },
+            follow=True
+        )
+        self.assertContains(response, 'already taken', status_code=200)
+        
 
 class BucketlistTestCase(SetUpMixin, TestCase):
-    """Test that the user can login and make a bucketlist
+    """Test that the user can carry out actions on bucketlist
     """
     def test_that_user_can_post_to_bucketlist_route(self):
-        response = self.client.post(
-                reverse('app.login'),
-                data=self.login_data
-            )
-        self.assertEqual(response.status_code, 302)
+        """Ensures that user can make new bucketlist
+        """
+        response = self.client.login(**self.login_data)
+        
+        self.assertEqual(response, True)
 
         bucketlist_data = {'name': 'Before I get married'}
         response = self.client.post(
-                reverse('app.bucketlist'),
-                data=bucketlist_data
+                reverse('app.bucketlist.create'),
+                bucketlist_data,
+                follow=True
             )
-        self.assertEqual(response.status_code == 302)
-        self.assertIn(bucketlist_data['name'], response.body)
+        self.assertEqual(response.status_code, 200)
+
+        self.assertContains(
+            response, bucketlist_data['name'], status_code=200
+        )
+        self.client.logout()
+
+    def test_that_user_can_delete_bucketlist(self):
+        """Ensures that user can delete bucketlist
+        """
+        response = self.client.post(
+                reverse('app.login'),
+                self.login_data,
+                follow=True
+            )
+        self.assertEqual(response.status_code, 200)
+        
+        bucketlist_data = {'name': 'Before I get married'}
+        response = self.client.post(
+                reverse('app.bucketlist.create'),
+                bucketlist_data,
+                follow=True
+            )
+        self.assertEqual(response.status_code, 200)
+        
+        response = self.client.post(
+                    reverse('app.bucketlist.delete', kwargs={'pk': 1}),
+                    bucketlist_data, follow=True
+            )
+        self.assertNotContains(response,
+                               bucketlist_data['name'],
+                               status_code=200)
+        
+    def test_that_user_can_update_bucketlist(self):
+        """Ensures that user can update bucketlist
+        """
+        response = self.client.post(
+                reverse('app.login'),
+                self.login_data
+            )
+        self.assertEqual(response.status_code, 302)
+        
+        bucketlist_data = {'name': 'Before I get married'}
+        response = self.client.post(
+                reverse('app.bucketlist.create'),
+                bucketlist_data
+            )
+        self.assertEqual(response.status_code, 302)
+        
+        bucketlist_data['name'] = 'Before I kick the bucket'
+        response = self.client.post(
+                reverse('app.bucketlist.update',
+                kwargs={'pk':1}
+            ),
+                bucketlist_data,
+                follow=True
+            )
+        self.assertContains(
+            response,
+            bucketlist_data['name'],
+            status_code=200
+        )
+
+    def test_that_user_can_read_bucketlist(self):
+        """Ensures that user can read lists of bucketlists
+        """
+        response = self.client.post(
+                 reverse('app.login'),
+                 self.login_data,
+                 follow=True
+            )
+        self.assertEqual(response.status_code, 200)
+        
+        bucketlist_data = {'name': 'Before I get married'}
+        response = self.client.post(
+                reverse('app.bucketlist.create'),
+                bucketlist_data,
+                follow=True
+            )
+        self.assertEqual(response.status_code, 200)
+        
+        response = self.client.get(reverse('app.bucketlists'))
+        
+        self.assertContains(response,
+                            bucketlist_data['name'],
+                            status_code=200)
+
+        
+class BucketlistItemTestCase(SetUpMixin, TestCase):
+    """Test that user can carry out actions on bucketlist items.
+    """
+    def test_that_user_can_create_new_bucketlistitem(self):
+        """Ensures that user can make new bucketlist item
+        """
+        response = self.client.login(**self.login_data)
+        
+        self.assertEqual(response, True)
+
+        bucketlist_data = {'name': 'Before I get married'}
+        response = self.client.post(
+                reverse('app.bucketlist.create'),
+                bucketlist_data,
+                follow=True
+            )
+
+        self.assertContains(
+            response, bucketlist_data['name'], status_code=200
+        )
+        
+        bucketlistitem_data = {'name': 'Get a dog', 'done': True}
+        response = self.client.post(
+                reverse('app.bucketlist.item.create', kwargs={'pk': 1}),
+                bucketlistitem_data,
+                follow=True
+            )
+        
+        self.assertContains(
+                response, bucketlistitem_data['name'], status_code=200
+            )
+        self.client.logout()
+
+    def test_that_user_can_delete_bucketlistitem(self):
+        """Ensures that user can delete bucketlist item
+        """
+        response = self.client.post(
+                reverse('app.login'),
+                self.login_data,
+                follow=True
+            )
+        self.assertEqual(response.status_code, 200)
+        
+        bucketlist_data = {'name': 'Before I get married'}
+        response = self.client.post(
+                reverse('app.bucketlist.create'),
+                bucketlist_data,
+                follow=True
+            )
+        self.assertContains(
+            response, bucketlist_data['name'], status_code=200
+        )
+        
+        bucketlistitem_data = {'name': 'Get a dog', 'done': True}
+        response = self.client.post(
+                reverse('app.bucketlist.item.create', kwargs={'pk': 1}),
+                bucketlistitem_data,
+                follow=True
+            )
+        
+        self.assertContains(
+                response, bucketlistitem_data['name'], status_code=200
+            )
+        
+        response = self.client.post(
+                    reverse('app.bucketlist.item.delete',
+                            kwargs={'pk': 1, 'pk_item': 1}),
+                    bucketlist_data, follow=True
+            )
+        self.assertNotContains(response,
+                               bucketlistitem_data['name'],
+                               status_code=200)
+        
+    def test_that_user_can_update_bucketlistitem(self):
+        """Ensures that user can update bucketlist item
+        """
+        response = self.client.post(
+                reverse('app.login'),
+                self.login_data,
+                follow=True
+            )
+        self.assertEqual(response.status_code, 200)
+        
+        bucketlist_data = {'name': 'Before I get married'}
+        response = self.client.post(
+                reverse('app.bucketlist.create'),
+                bucketlist_data,
+                follow=True
+            )
+        self.assertContains(
+            response, bucketlist_data['name'], status_code=200
+        )
+        
+        bucketlistitem_data = {'name': 'Get a dog', 'done': True}
+        response = self.client.post(
+                reverse('app.bucketlist.item.create', kwargs={'pk': 1}),
+                bucketlistitem_data,
+                follow=True
+            )
+        
+        self.assertContains(
+                response, bucketlistitem_data['name'], status_code=200
+            )
+        
+        bucketlistitem_data['name'] = 'Get a cat'
+        response = self.client.post(
+                reverse('app.bucketlist.item.update',
+                kwargs={'pk':1, 'pk_item': 1}
+            ),
+                bucketlistitem_data,
+                follow=True
+            )
+        self.assertContains(
+            response,
+            bucketlistitem_data['name'],
+            status_code=200
+        )
