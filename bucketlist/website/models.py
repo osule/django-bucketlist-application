84af5b3d0552d3ec3ecad137fb5a3be6c9600579
Oluwafemi Sule <oluwafemi.sule@andela.com>
2015-11-12 15:56:15 +0100

    (feature) create abstract BaseModel having common fields; add search class method for record queries

--- bucketlist/website/models.py
+++ bucketlist/website/models.py
@@ -2,15 +2,41 @@
 
 from django.db import models
 from django.contrib.auth.models import User
+from .utils import normalize
 
-
-class Bucketlist(models.Model):
-    """A model representation of the Bucketlist table
+class BaseModel(models.Model):
+    """ An abstract model that has common information for
+    `Bucketlist` and `BucketlistItem`
     """
     name = models.CharField(max_length=255)
-    user = models.ForeignKey(User)
     date_created = models.DateTimeField(auto_now_add=True)
     date_modified = models.DateTimeField(auto_now=True)
+    class Meta:
+        abstract = True
+    
+    @classmethod
+    def search(cls, query_string):
+        query = None
+        query_terms = normalize(query_string)
+        for term in query_terms:
+            or_query = None # Query to search for a given term in each field
+            query_obj = models.Q(**{"name__icontains": term})
+            if or_query is None:
+                or_query = query_obj
+            else:
+                or_query = or_query | query_obj
+            if query is None:
+                query = or_query
+            else:
+                query = query & or_query
+        return cls.objects.filter(query).order_by('date_created')
+
+
+class Bucketlist(BaseModel):
+    """A model representation of the Bucketlist table
+    """
+    user = models.ForeignKey(User)
+    
 
     def bucketlistitem_count_done(self):
         """Gets the count of bucketlist items done
@@ -25,12 +51,9 @@ class Bucketlist(models.Model):
             bucketlist=self, done=False).count()
 
 
-class BucketlistItem(models.Model):
+class BucketlistItem(BaseModel):
     """A model representation of the Bucketlist item table
     """
-    name = models.CharField(max_length=255)
     done = models.BooleanField(default=False)
     user = models.ForeignKey(User)
     bucketlist = models.ForeignKey(Bucketlist)
-    date_created = models.DateTimeField(auto_now_add=True)
-    date_modified = models.DateTimeField(auto_now=True)
