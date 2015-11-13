a52e5f9cd5f1d08f12d107ffaca529bfaf8a5ef7
Oluwafemi Sule <oluwafemi.sule@andela.com>
2015-11-13 15:13:44 +0100

    (feature) add view handling logic for endpoints

---
+++ bucketlist/api/views.py
@@ -0,0 +1,76 @@
+from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth.models import User
+from django.shortcuts import get_object_or_404
+from rest_framework import generics, viewsets
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from .serializers import BucketlistSerializer, UserSerializer, \
+BucketlistWithItemsSerializer, BucketlistItemSerializer, \
+BucketlistItemCreateSerializer
+from website.models import Bucketlist, BucketlistItem
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    """defines the user view behavior
+    """
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class BucketlistList(generics.ListCreateAPIView):
+    """defines the bucketlist list view behaviour
+    """
+    serializer_class = BucketlistSerializer
+    permission_classes = (IsAuthenticated,)
+    paginate_by = 100
+    
+    def get_queryset(self):
+        q = self.request.GET.get('q', None)
+        if q:
+            return Bucketlist.search(q)
+        return Bucketlist.objects.filter(user=self.request.user)
+    
+    def perform_create(self, serializer):
+        serializer.save(user_id=self.request.user.id)
+
+        
+class BucketlistDetail(generics.RetrieveUpdateDestroyAPIView):
+    """defines the bucketlist detail view behaviour.
+    Comes with Read, Update and Delete.
+    """
+    queryset = Bucketlist
+    serializer_class = BucketlistWithItemsSerializer
+    permission_classes = (IsAuthenticated,)
+    
+    def get_query(self):
+        return get_object_or_404(Bucketlist, pk=self.kwargs.get('pk'))
+
+
+class BucketlistItemCreate(generics.ListCreateAPIView):
+    """defines the bucketlist create view behaviour.
+    """
+    model = BucketlistItem
+    serializer_class = BucketlistItemSerializer
+    
+    def get_queryset(self):
+        pk = self.kwargs.get('pk')
+        bucketlist = get_object_or_404(Bucketlist, pk=pk)
+        return BucketlistItem.objects.filter(
+            user=self.request.user,bucketlist=bucketlist)
+
+    def perform_create(self, serializer):
+        pk = self.kwargs.get('pk')
+        bucketlist = get_object_or_404(
+            Bucketlist, pk=pk, user_id=self.request.user.id)
+        serializer.save(bucketlist=bucketlist, user_id=self.request.user.id)
+
+class BucketlistItemDetail(generics.RetrieveUpdateDestroyAPIView):
+    """defines the bucketlist detail view behaviour.
+    Comes with Read, Update, and Delete.
+    """
+    queryset = BucketlistItem
+    serializer_class = BucketlistItemSerializer
+    
+    def get_object(self):
+        pk_item = self.kwargs.get('pk_item')
+        return get_object_or_404(BucketlistItem, pk=pk_item)
+
