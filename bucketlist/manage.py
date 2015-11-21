d44afe46a1b62b5a7a1523884686aa0c6576c348
Oluwafemi Sule <oluwafemi.sule@andela.com>
2015-11-21 21:28:32 +0100

    (fix) use DJANGO_SETTINGS_MODULE set in environment variable

--- bucketlist/manage.py
+++ bucketlist/manage.py
@@ -3,12 +3,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-    if os.getenv('CIRCLE_CI'):
-        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings.testing")
-    elif os.getenv('HEROKU'):
-        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings.staging")
-    else:
-        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings.development")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", 'settings.development')
 
     from django.core.management import execute_from_command_line
 
