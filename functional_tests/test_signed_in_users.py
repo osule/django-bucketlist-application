b30d14767bb97a57e2a1ef589b548039b5bc2dcb
Oluwafemi Sule <oluwafemi.sule@andela.com>
2015-11-22 13:49:33 +0100

    (chore) add pragma to uncover function

--- functional_tests/test_signed_in_users.py
+++ functional_tests/test_signed_in_users.py
@@ -19,15 +19,15 @@ class HomeSigninUserTest(LiveServerTestCase):
             'username': 'basil',
             'password': 'some_really_strong_password'
         }
-        
+
     def setUp(self):
         User.objects.create_user(**self.login_data)
         # sign in user
         self.browser.get('%s%s' % (self.live_server_url, '/'))
         self.browser.find_element_by_id(
-            "signInUsername").send_keys(self.login_data['username'])  
+            "signInUsername").send_keys(self.login_data['username'])
         self.browser.find_element_by_id(
-            "signInPassword").send_keys('some_really_strong_password')    
+            "signInPassword").send_keys('some_really_strong_password')
         self.browser.find_element_by_id(
             "signInBtn"
             ).click()
@@ -40,7 +40,7 @@ class HomeSigninUserTest(LiveServerTestCase):
         super(HomeSigninUserTest, cls).tearDownClass()
 
     def _get_full_url(self, namespace):
-        return self.live_server_url + reverse(namespace)
+        return self.live_server_url + reverse(namespace)  # pragma: no cover
 
     def test_dashboard(self):
         """Test that Bucketlist link is present in Dashboard.
@@ -48,10 +48,10 @@ class HomeSigninUserTest(LiveServerTestCase):
         self.assertIn(
             'Dashboard',
             self.browser.find_element_by_tag_name('body').text)
-        
+
         self.assertIn(
-            'Bucketlists', 
-             self.browser.find_element_by_css_selector('section ul li a').text)
+            'Bucketlists',
+            self.browser.find_element_by_css_selector('section ul li a').text)
 
     def test_can_navigate_to_bucketlist_details(self):
         """Test that Bucketlist detail page can be navigated.
