b57077f2207f4a0b35ebc5cbb597640705cb50d1
Oluwafemi Sule <oluwafemi.sule@andela.com>
2015-11-26 11:36:41 +0100

    (chore) update docstring

--- bucketlist/website/templatetags/tag_library.py
+++ bucketlist/website/templatetags/tag_library.py
@@ -4,6 +4,7 @@ from django import template
 
 register = template.Library()
 
-@register.filter(name="to_str")
+@register.filter(name="to_str")  # registers the `to_str` filter
 def to_str(value):
+    """converts any value to a string representation"""
     return str(value)
